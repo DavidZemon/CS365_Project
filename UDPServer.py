8e03b63d5185bd54fea24c6781857a9c92652daf
David Zemon <david@stlswedespeed.com>
2013-10-03 14:50:40 -0500

    It's my first Python 3 code! So cool!

---
+++ UDPServer.py
@@ -0,0 +1,24 @@
+#/usr/bin/python3
+# Author:  David Zemon
+# Project: Project1
+#
+# Created with: PyCharm Community Edition
+
+"""
+@description: Implement the client side of http to interact with the server you have developed in the previous
+assignment. You do not need to implement the function to display the received object, just save it as a local file.
+"""
+
+from socket import socket, AF_INET, SOCK_DGRAM
+
+serverPort = 12000
+
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+
+print("The server is ready to receive")
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
