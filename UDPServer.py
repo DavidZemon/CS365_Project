7a32fd637366c98d6cd3f90993ac6ef799d51cb4
David Zemon <david@stlswedespeed.com>
2013-10-03 14:51:59 -0500

    Modified server to ensure message.upper() works correctly

--- UDPServer.py
+++ UDPServer.py
@@ -20,5 +20,6 @@ print("The server is ready to receive")
 
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
+    message = message.decode('utf-8')
     modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddress)
+    serverSocket.sendto(modifiedMessage.encode('utf-8'), clientAddress)
